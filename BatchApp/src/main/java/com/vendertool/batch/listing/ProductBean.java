44e8947d939c88520ec0d78c697ce24e3e1af2b0
Girish Kodaganti <girish.kodaganti@gmail.com>
2013-07-13 11:24:45 -0700

    Adding Product bean

---
+++ BatchApp/src/main/java/com/vendertool/batch/listing/ProductBean.java
@@ -0,0 +1,26 @@
+package com.vendertool.batch.listing;
+
+public class ProductBean {
+
+	private String handle;
+	private String sku;
+	private String type;
+	public String getHandle() {
+		return handle;
+	}
+	public void setHandle(String handle) {
+		this.handle = handle;
+	}
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
